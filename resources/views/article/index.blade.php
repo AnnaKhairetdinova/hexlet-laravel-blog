2b9122090ccf3161640f298b0b911ad23bcf8482
Anna Khairetdinova <AnnKhairetdinova@yandex.ru>
2024-07-03 18:35:33 +0500

    crud articles

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,7 +3,11 @@
 @section('content')
     <h1>Список статей</h1>
     @foreach ($articles as $article)
-        <h2>{{$article->name}}</h2>
+        <a href="{{ route('articles.show', $article) }}">{{$article->name}}</a>
+
+        <a href="{{ route('articles.edit', $article) }}">Обновить категорию</a>
+
+        <a href="{{ route('articles.destroy', $article) }}" data-confirm="Вы уверены?" data-method="delete" rel="nofollow">Удалить</a>
         <div>{{$article->body}}</div>
     @endforeach
 @endsection
